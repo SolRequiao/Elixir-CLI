8cc43d510e99fe262968f28b0237441f373cb73b
Sol <solrequiao@gmail.com>
2024-07-19 20:43:30 -0300

    docs: adicionar mensagem de aviso sobre a funcionalidade do programa
    Adiciona uma linha de aviso para informar ao usuário que o programa tem o intuito de receber nomes e que, após a inserção, listará os nomes com suas respectivas numerações romanas.

--- lib/desafio_cli.ex
+++ lib/desafio_cli.ex
@@ -7,6 +7,7 @@ defmodule DesafioCli do
   A função main é o ponto de entrada da CLI. Ela inicia a coleta de nomes.
   """
   def main(_args) do
+    IO.puts("Esse programa tem o intuito de receber nomes. Após a inserção dos nomes, o programa irá listá-los com suas respectivas numerações romanas.")
     ColetarNomes.coletar_nomes([])
   end
 end
